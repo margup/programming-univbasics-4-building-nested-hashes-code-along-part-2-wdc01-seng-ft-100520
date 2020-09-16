b39961ecf4fa111f242389739ca6adfe30a6ef4d
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-16 22:07:41 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -55,11 +55,10 @@ def add_characters
    }
 
    # Write code below that does the following programmatically:
-   epic_tragedy[:additional_characters] = []
-   prince = { name: "Prince Escalus" }
-   apothecary = { name: "Apothecary" }
-   epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
-  epic_tragedy[:additional_characters] << { name: "Apothecary" }
+epic_tragedy[:additional_characters] = [
+  { name: "Prince Escalus" },
+  { name: "Apothecary" }
+]
    
    
    #
