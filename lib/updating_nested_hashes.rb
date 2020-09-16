8fe33aa226987d4829994d5c6b8d4e9283f84933
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-16 22:04:34 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -55,6 +55,7 @@ def add_characters
    }
 
    # Write code below that does the following programmatically:
+   
    #
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
    # - Add two hashes to this array, both with a :name key.
